77644cc26be3faa50c7441f9770834c3e8bf1292
LeonarDev <leonardo.majevski@gmail.com>
2021-09-22 20:41:30 -0300

    Adiciona outras maneiras de se declarar utilizando Comparison

--- 1-Comparison/Program.cs
+++ 1-Comparison/Program.cs
@@ -8,18 +8,33 @@ namespace _1_Comparison
     {
         static void Main(string[] args)
         {
+            // INLINE LAMBDA EXPRESSION
             List<Product> list = new List<Product>();
 
             list.Add(new Product("TV", 900.00));
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
+            //       INLINE LAMBDA EXPRESSION
             list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            //       LAMBDA EXPRESSION TO DELEGATE VARIABLE
+            // Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+            // list.Sort(comp);
+
+            //       METHOD REFERENCE TO DELEGATE VARIABLE
+            // Comparison<Product> comp = CompareProducts;
+            // list.Sort(comp);
 
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
+        }
+
+        static int CompareProducts(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
--- README.md
+++ README.md
@@ -287,4 +287,14 @@ namespace Course
 <br>
 
 ## Predicate (System)
-- Representa...
+Representa um método que recebe um objeto do tipo T e retorna um valor booleano
+
+https://msdn.microsoft.com/en-us/library/bfcke1bz%28v=vs.110%29.aspx
+
+
+```c#
+public delegate bool Predicate<in T>(T obj);
+```
+
+<img src='./img/3.png'>
+
