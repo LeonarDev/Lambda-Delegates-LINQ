7d8caba4a7e81545d2271972815262a6964e4a96
LeonarDev <leonardo.majevski@gmail.com>
2021-09-29 22:14:58 -0300

    Exercicio 1

--- 8-LINQ-com-Lambda/Program.cs
+++ 8-LINQ-com-Lambda/Program.cs
@@ -38,23 +38,54 @@ namespace _8_LINQ_com_Lambda
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900
+                select p;
             Print("TIER 1 AND PRICE < 900:", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
             Print("NAMES OF PRODCTS FROM TOOLS:", r2);
 
             //var r3 = products.Where(p => p.Name[0] == 'C');
-            var r3 = products.Where(p => p.Name.StartsWith('C')).Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            //var r3 = products.Where(p => p.Name.StartsWith('C')).Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name
+                };
             Print("PRODUCTS WHERE THE NAME STARTS WITH 'C':", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
+
             Print("TIER 1 ORDERED BY PRICE THEN BY NAME:", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 =
+                (from p in r4
+                 select p)
+                 .Skip(2)
+                 .Take(4);
+
             Print("TIER 1 ORDERED BY PRICE THEN BY NAME | SKIP 2 TAKE 4:", r5);
 
-            var r6 = products.FirstOrDefault();
+            //var r6 = products.FirstOrDefault();
+            var r6 = (from p in products select p).FirstOrDefault();
             Console.WriteLine("First or default test1: " + r6);
             var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
             Console.WriteLine("First or default test2: " + r7);
@@ -83,7 +114,10 @@ namespace _8_LINQ_com_Lambda
             Console.WriteLine();
 
             Console.WriteLine("GROUP BY: ");
-            var r17 = products.GroupBy(p => p.Category);
+            //var r17 = products.GroupBy(p => p.Category);
+            var r17 =
+                from p in products
+                group p by p.Category;
             foreach (IGrouping<Category, Product> group in r17)
             {
                 Console.WriteLine("____________________");
